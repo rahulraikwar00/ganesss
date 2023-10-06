70802eb3c5df11a7cb3e8ab3ccdc5bf79c5a526b
Rahul raikwar <rr200636@gmail.com>
2023-10-06 18:08:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,60 @@
 document.addEventListener("DOMContentLoaded", function() {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-    const clearBtn = document.getElementById("clear-btn");
-    const directionDiv = document.getElementById("direction");
-    let painting = false;
     let stepCount = 0;
-    let deviceDirection = 0; // Variable to store device direction in degrees
-
-    function startPosition(e) {
-        painting = true;
-        draw(e);
+    let deviceDirection = 0;
+    let startSelected = false;
+    let startX = 0;
+    let startY = 0;
+  
+    function drawDot(x, y) {
+      ctx.beginPath();
+      ctx.arc(x, y, 5, 0, Math.PI * 2);
+      ctx.fillStyle = "blue";
+      ctx.fill();
+      ctx.closePath();
     }
-
-    function endPosition() {
-        painting = false;
-        ctx.beginPath();
-    }
-
-    function draw(e) {
-        if (!painting) return;
-        ctx.lineWidth = 5;
-        ctx.lineCap = "round";
-
-        ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
-    }
-
+  
     function handleDeviceMotion(event) {
-        stepCount = event.accelerationIncludingGravity.x > 10 ? stepCount + 1 : stepCount;
-        document.getElementById("step-count").innerText = `Step Count: ${stepCount}`;
+      stepCount = event.accelerationIncludingGravity.x > 0 ? stepCount + 1 : stepCount;
+      document.getElementById("step-count").innerText = `Step Count: ${stepCount}`;
     }
-
+  
     function handleDeviceOrientation(event) {
-        deviceDirection = event.alpha; // Get device orientation in degrees (0 to 360)
-        directionDiv.innerText = `Device Direction: ${deviceDirection.toFixed(2)}°`;
+      deviceDirection = event.alpha || 0;
+      document.getElementById("direction").innerText = `Device Direction: ${deviceDirection.toFixed(2)}°`;
     }
-
-    canvas.addEventListener("mousedown", startPosition);
-    canvas.addEventListener("mouseup", endPosition);
-    canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("mouseout", endPosition);
-
-    clearBtn.addEventListener("click", function() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        stepCount = 0;
-        document.getElementById("step-count").innerText = `Step Count: 0`;
-        directionDiv.innerText = `Device Direction: 0°`;
-    });
-
+  
     if (window.DeviceMotionEvent) {
-        window.addEventListener("devicemotion", handleDeviceMotion, false);
+      window.addEventListener("devicemotion", handleDeviceMotion, false);
     } else {
-        document.getElementById("step-count").innerText = "Step Counter not supported on this device/browser.";
+      document.getElementById("step-count").innerText = "Step Counter not supported on this device/browser.";
     }
-
+  
     if (window.DeviceOrientationEvent) {
-        window.addEventListener("deviceorientation", handleDeviceOrientation, false);
+      window.addEventListener("deviceorientation", handleDeviceOrientation, false);
     } else {
-        directionDiv.innerText = "Device Orientation not supported on this device/browser.";
+      document.getElementById("direction").innerText = "Device Orientation not supported on this device/browser.";
     }
-});
+  
+    canvas.addEventListener("mousedown", function(event) {
+      if (!startSelected) {
+        const rect = canvas.getBoundingClientRect();
+        startX = event.clientX - rect.left;
+        startY = event.clientY - rect.top;
+        startSelected = true;
+        document.getElementById("start-point").innerText = `Start Point: (${startX.toFixed(2)}, ${startY.toFixed(2)})`;
+      } else {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+  
+        const radians = (deviceDirection * Math.PI) / 180;
+        const newX = x + stepCount * Math.cos(radians) * 10;
+        const newY = y + stepCount * Math.sin(radians) * 10;
+  
+        drawDot(newX, newY);
+      }
+    });
+  });
+  
