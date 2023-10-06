17ab489f0741903b21ed3a8c6bc6ec3f4e86a7e2
rahulraikwar00 <rr200636@gmail.com>
2023-10-07 00:23:49 +0530

    jasdfasdasdfa

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const rose = document.getElementById('wind-rose')
 const heading = document.getElementById('heading')
+
 const stepCountDisplay = document.getElementById('step-count');
 
 let compass
 let stepCount = 0;
+
 const isIOS = (
   navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
   navigator.userAgent.match(/AppleWebKit/)
@@ -26,27 +28,27 @@ function initCompass() {
 
 function handler(e) {
   console.log(e)
-  compass = e.webkitCompassHeading  || Math.abs(e.alpha - 360)
-//   rose.style.transform = `translate(-50%,-50%) rotate(${-compass}deg)`
+  compass = e.webkitCompassHeading || Math.abs(e.alpha - 360)
+  rose.style.transform = `translate(-50%,-50%) rotate(${-compass}deg)`
   heading.innerText = Math.round(compass) + ' deg'
 }
 
+
+
 // step counter
 function initStepCounter() {
-    if (window.DeviceMotionEvent) {
-      window.addEventListener('devicemotion', handleMotion, false);
-      console.log('yes step conter sppurted')
-    } else {
-      console.log('Step Counter not supported on this device/browser.');
-    }
+  if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', handleMotion, false);
+    console.log('yes step conter sppurted')
+  } else {
+    console.log('Step Counter not supported on this device/browser.');
   }
-  
-  function handleMotion(event) {
-    stepCount = event.accelerationIncludingGravity.x > 0 ? stepCount + 1 : stepCount;
-    // Update the step count display
-    stepCountDisplay.innerText = `Step Count: ${stepCount}`;
-  }
+}
 
+function handleMotion(event) {
+  stepCount = event.accelerationIncludingGravity.x > 0 ? stepCount + 1 : stepCount;
+  // Update the step count display
+  stepCountDisplay.innerText = `Step Count: ${stepCount}`;
+}
 initCompass()
-initStepCounter();
-
+initStepCounter()
